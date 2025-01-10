a93a844063af88696faa999f7adc6215174f28c1
Erik Hartl <erikhartl80@gmail.com>
2025-01-10 20:47:25 +0100

    Sources for easy, medium and hard tasks. Documentation of tasks and sources in Sources.ods

---
+++ src/Hard/44. Wildcard Matching/user_solution_2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        i, j = 0, 0
+        last_match, star = 0, -1
+
+        while i < len(s):
+            if j < len(p) and (s[i] == p[j] or p[j] == '?'):
+                i += 1
+                j += 1
+            elif j < len(p) and p[j] == '*':
+                last_match = i
+                star = j
+                j += 1
+            elif star != -1:
+                j = star + 1
+                i = last_match + 1
+                last_match += 1
+            else:
+                return False
+
+        while j < len(p) and p[j] == '*':
+            j += 1
+
+        return j == len(p)
+        ##Upvote me If it helps
---
+++ src/Hard/65. Valid Number/user_solution_1.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        pattern = r'^[+-]?(\d+\.?\d*|\.\d+)([eE][+-]?\d+)?
+        return bool(re.match(pattern,s))
+
